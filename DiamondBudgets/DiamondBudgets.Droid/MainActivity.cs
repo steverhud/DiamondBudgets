90124e3bd6d58cc809610bbd6f9b4b3d8f11d81c
Stephen Hudson <stephen.hudson@diamondmunicipal.com>
2016-08-31 10:22:41 -0400

    something

--- DiamondBudgets/DiamondBudgets.Droid/MainActivity.cs
+++ DiamondBudgets/DiamondBudgets.Droid/MainActivity.cs
@@ -9,7 +9,7 @@ using Android.OS;
 
 namespace DiamondBudgets.Droid
 {
-    [Activity(Label = "DiamondBudgets", Icon = "@drawable/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    [Activity(Label = "Diamond Budgets", Icon = "@drawable/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
         protected override void OnCreate(Bundle bundle)
